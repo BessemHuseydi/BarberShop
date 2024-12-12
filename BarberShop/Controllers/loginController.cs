d97620b27d53d9f2eda1975a48248ab679164b20
BessemHuseydi <bessem112005@gmail.com>
2024-12-12 17:58:47 +0300

    css ve view ekleme

---
+++ BarberShop/Controllers/loginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BarberShop.Controllers
+{
+    public class loginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult uyeol()
+        {
+            return View();
+        }
+    }
+}
